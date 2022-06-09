8c8e8edf5ac1da03c650333b23e96f38b3099122
ludoguenet <ludovicguenet@gmx.com>
2022-06-09 10:27:34 +0200

    DashboardController invokable setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get(
+        'dashboard',
+        DashboardController::class
+    )->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
