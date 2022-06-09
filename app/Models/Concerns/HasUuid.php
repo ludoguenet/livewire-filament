3bb891af9e3349b1ade56368d738dc3b19d81433
ludoguenet <ludovicguenet@gmx.com>
2022-06-09 09:01:47 +0200

    Adding HasUuid trait

---
+++ app/Models/Concerns/HasUuid.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Concerns;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+trait HasUuid
+{
+    public static function bootHasUuid(): void
+    {
+        static::creating( fn(Model $model) =>
+            $model->uuid = Str::uuid()->toString(),
+        );
+    }
+}
